6ece08d0318611a31e2a0b63ae51f5a3af33cf3b
EC2 Default User <ec2-user@ip-172-31-42-58.ap-northeast-1.compute.internal>
2024-09-09 13:08:48 +0000

    コメント機能追加

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentsController < ApplicationController
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    comment = current_user.comments.new(comment_params)
+    comment.recipe_id = recipe.id
+    comment.save
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
        
   has_many :favorites, dependent: :destroy
   has_many :recipes, dependent: :destroy
+  has_many :commetns, dependent: :destroy
   
   def self.search_for(content, method)
     if method == 'perfect'
