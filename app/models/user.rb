8baae6904057262afbcb6295deb65c41d279ce33
EC2 Default User <ec2-user@ip-172-31-42-58.ap-northeast-1.compute.internal>
2024-09-09 09:11:34 +0000

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,18 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :recipes, dependent: :destroy
   
-  
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
   validates :name, presence: true
   validates :nickname, presence: true
   validates :email, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,14 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about'
   get 'users/mypage'
+  
   resources :recipes, only: [:new, :create, :index, :show, :edit, :update, :destroy] do 
    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:mypage, :edit, :show, :update, :destroy] do
     resources :favorite, only:[:index]
   end
+  
+  get '/search', to: 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
